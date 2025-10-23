71a31b5e0fa43ba4e0652e807558daa0a248f3fb
Harry Hernandez Arias <132928917+HarryHndz@users.noreply.github.com>
2025-10-23 10:16:07 -0600

    create repository app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  return (
+    <div className=''>
+      <h1 className="text-3xl font-bold">
+        News Fake App
+      </h1>
+       
+    </div>
+  )
+}
+
+export default App
